17690f8f073d51c87c731a2243a2b64076246b69
arf1e <anotheruselessbox@gmail.com>
2019-04-15 00:31:02 +0300

    ahh seems like you are from England

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,19 +1,68 @@
+/*
+  Компонент-обертка для всех страниц в проекте
+*/
 import React from 'react';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 import Header from './Header';
 import Meta from './Meta';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next'
+  }
+
+  a {
+    text-decoration: none;
+    color: ${theme.black}
+  }
+`;
+
+const StyledPage = styled.div``;
+
+const Inner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 class Page extends React.Component {
   state = {};
 
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        <p>Хэдер бля</p>
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <p>Хэдер бля</p>
+          <Inner>{this.props.children}</Inner>
+          <GlobalStyle />
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
