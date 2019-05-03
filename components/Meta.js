75f9543421787638d5b79efb3391f7c9e63c654d
arf1e <anotheruselessbox@gmail.com>
2019-05-03 15:17:53 +0300

    meta

--- components/Meta.js
+++ components/Meta.js
@@ -5,7 +5,7 @@ import Head from 'next/head';
 const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta charSet="utf-8" />
+    <meta charset="utf-8" />
     <title>Gouda</title>
   </Head>
 );
